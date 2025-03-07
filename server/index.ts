5215e5110cc26b5a4091ad1a368eab0e9bd61e76
PaulDRev <paul@dentalrevenue.com>
2025-03-07 15:34:50 -0500

    Dev server runs

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import { createServer } from "http";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 const app = express();
 app.use(express.json());
