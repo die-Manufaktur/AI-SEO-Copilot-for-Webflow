40fb006db496061dc34e55ad1b1321d76489af16
PaulDRev <paul@dentalrevenue.com>
2025-03-10 17:39:17 -0400

    Bad dev commits to main

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/vite'
 
 // Get the directory name
 const __filename = fileURLToPath(import.meta.url);
@@ -9,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   root: path.resolve(__dirname, 'client'), // Set the root directory to 'client'
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: path.resolve(__dirname, 'public'), // Change output directory to 'public'
     emptyOutDir: true, // Ensure the output directory is emptied before building
